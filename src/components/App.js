52d6f9417d5ee71f3b6ecceb36b3b2789682c60a
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-10-27 06:13:49 +0530

    add

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+import React,{Component} from 'react';
+import {SignIn} from './';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       <SignIn/>
+      </div>
+    )
+  }
+}
---
+++ src/components/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class SignIn extends Component {
+  
+         render() {
+                  return (
+                           <div className="sign-form">
+                               <h1>Sign In</h1>
+                          <button>Sign In With Google</button>
+                           </div>
+                  )
+         }
+}
+// export default SignIn;
