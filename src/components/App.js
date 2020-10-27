145faf4b991b85cbda86c330637bf0c814fafe3e
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-10-27 08:16:23 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 // import './App.css';
 import React,{Component} from 'react';
 import {SignIn} from './';
+import {Switch,Route} from 'react-router-dom'
+function Home(){
+  return(
+    <div>Home</div>
+  )
+}
+function Some() {
+  return(
+  <div>Some</div>
+  )
+}
+
+
+
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
-       <SignIn/>
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/some" component={Some} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,11 +4,11 @@ export default class SignIn extends Component {
   
          render() {
                   return (
-                           <div className="sign-form">
-                               <h1>Sign In</h1>
-                          <button>Sign In With Google</button>
-                           </div>
-                  )
+                    <div className="sign-form">
+                      <h1>Sign In/Sign Up</h1>
+                      <button>Sign In With Google/Sign Up With Google</button>
+                    </div>
+                  );
          }
 }
 // export default SignIn;
