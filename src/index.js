2b4605c89ec5ffa8cd1785a6d8e77f6280e3ad61
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-10-28 21:25:10 +0530

    add

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
-import {App} from './components'
+import {App} from './components';
+import UserProvider from './providers/UserProvider';
 
 ReactDOM.render(
+  <UserProvider>
   <BrowserRouter>
-  <App />
-  </BrowserRouter>,
-  
+  <App/>
+  </BrowserRouter>
+  </UserProvider>,
   document.getElementById('root')
 );
 
--- src/providers/UserProvider.js
+++ src/providers/UserProvider.js
@@ -1,13 +1,25 @@
 import React, { Component,createContext } from 'react';
+import { auth,createOrGetUserProfileDocument} from '../firebase';
 const InitialUserState={user:null,loading:false}
 
 export const UserContext=createContext(InitialUserState)
 class UserProvider extends Component {
+         state=InitialUserState;
+         async componentDidMount(){ ///lifecycle 
+       auth.onAuthStateChanged(async(userAuth)=>{
+console.log('yurororo',userAuth);
+if(userAuth){
+         const userref=await createOrGetUserProfileDocument(userAuth);
+}
+       });
+         }
          render() {
                   return (
-                           <div>
-                                    
-                           </div>
+   <UserContext.Provider value={this.state}>
+            {this.props.children}
+   </UserContext.Provider>
                   )
          }
 }
+export default UserProvider;
+
